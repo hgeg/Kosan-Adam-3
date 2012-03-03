2d590a3ba0e479063047111688e5c5dacae0873f
Can Bülbül <alicanblbl@gmail.com>
2012-03-03 22:19:18 +0200

    .....

--- gameworld.c
+++ gameworld.c
@@ -350,8 +350,8 @@ GameWorldC::GameWorldC(const char *map)
          staticObjects[staticCount] = *objects[del];
          staticObjects[staticCount].x = x*64;
          staticObjects[staticCount].y = y*64;
-         staticObjects[staticCount].collisionRect.x = x*64;
-         staticObjects[staticCount].collisionRect.y = y*64;
+         staticObjects[staticCount].collisionRect.x = x*64 + staticObjects[staticCount].colOffX;
+         staticObjects[staticCount].collisionRect.y = y*64 + staticObjects[staticCount].colOffY;
          staticCount++;
          /*cout << staticCount << endl;*/
        }
